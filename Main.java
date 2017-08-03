485c091265f5f8ca0251284f4593e3a626a91eef
sarath307 <sarathkumarr307@gmail.com>
2017-08-03 09:36:18 +0530

    added

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main(String[] args) {
+System.out.println("Start");
+for (int i = 1; i <= 40; i++) {
+System.out.println(i);
+}
+System.out.println("End");
+}
+}
